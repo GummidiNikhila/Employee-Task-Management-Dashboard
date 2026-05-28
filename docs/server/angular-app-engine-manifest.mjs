008936d2d1cbcc2ab5fbc717622a81fbcdb2bb8b
Nikhila gummidi <nikhilagummidi6@gmail.com>
2026-05-28 10:45:27 +0530

    deploy

---
+++ docs/server/angular-app-engine-manifest.mjs
@@ -0,0 +1,11 @@
+
+export default {
+  basePath: '/Employee-Task-Management-Dashboard',
+  allowedHosts: [],
+  supportedLocales: {
+  "en-US": ""
+},
+  entryPoints: {
+    '': () => import('./main.server.mjs')
+  },
+};
